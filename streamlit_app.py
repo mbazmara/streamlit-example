15107f78ef26a8a55b3c1cb9dfaf1ee944f097f7
mbazmara <47808895+mbazmara@users.noreply.github.com>
2022-04-02 19:57:09 +0430

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,68 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.set_page_config(page_title='Clustering your data', page_icon=':chart_with_upwards_trend:', layout='wide')
+st.title("Clustering any DataSet you want")
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# ---- Functions ----
+def categorize(data):
+    for f_type in data.columns:
+        if isinstance(data[f_type][0], str):
+            data[f_type] = data[f_type].astype('category')
+            data[f_type] = data[f_type].cat.codes
+        else:
+            pass
+    return data
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
+def visualize(data, n_clusters,x,y):
+    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+    model = KMeans(n_clusters=number_of_clusters)
+    clusters = model.fit_transform(input_data)
+    input_data['cluster'] = model.labels_
+    for i in range(n_clusters):
+        clustered_data = data[data['cluster']==i]
+        plt.scatter(clustered_data[x], clustered_data[y], c=colors[i-1])
+    plt.title('Clustered Database that you uploaded')
+    plt.xlabel(x)
+    plt.ylabel(y)
+    st.pyplot()
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+def del_useless_features(data, number_of_instances):
+    for f_type in data.columns:
+        if data[f_type].nunique() == number_of_instances:
+            data = data.drop(columns=[f_type])
+            print('Kbooom')
+    return data
+
+
+# ---- Input Data ----
+x = st.sidebar.file_uploader("Pick a file", type=['csv', 'xlsx'])
+if x is not None:
+    data = pd.read_csv(x)
+    number_of_instances = data.shape[1]
+    input_data = data
+    #input_data = del_useless_features(data, number_of_instances)
+    numeric_features = list(input_data.select_dtypes(['int', 'float']).columns)
+    categorize(input_data)
+
+    # ---- SlideBar ----
+    #st.sidebar.subheader('Give us your data to cluster')
+    number_of_clusters = st.sidebar.slider(label='Number of clusters', min_value=2, max_value=8)
+    f1 = st.sidebar.selectbox(label='X Numeric Feature:', options=numeric_features)
+    f2 = st.sidebar.selectbox(label='Y Numeric Feature:', options=numeric_features)
+
+    st.write('---')
+    with st.container():
+        left, right = st.columns(2)
+        with left:
+            st.subheader("first 5 insctances of your dataset")
+            st.write(data.head(5))
+        with right:
+            visualize(input_data, number_of_clusters, f1, f2)
+    st.write('---')
+    #Visualize(input_data, number_of_clusters)
